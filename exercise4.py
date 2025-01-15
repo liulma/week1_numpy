89c50c965876036bf3baef67b2e3cb175d77a0d5
liulma <linda.ulma@skillio.ai>
2025-01-15 11:17:12 +0200

    2D array, rounded down

---
+++ exercise4.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Exercise 4: 2D array with two lists. Round down each element
+
+arr_2d = np.array([[3.14, 2.718, 1.414], [5.55, 6.66, 7.77]])
+
+floored_values = np.floor(arr_2d)
+
+print("Float array:")
+print(arr_2d)
+print()
+print("Floored values:")
+print(floored_values)
